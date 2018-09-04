197b2efc50c0b32b9c7745eba7bc6e4d6f4e5618
Avinash <41838739+avinash4321@users.noreply.github.com>
2018-09-04 22:16:36 +0530

    Chapter 1 - exercise 1

---
+++ R1.1.py
@@ -0,0 +1,19 @@
+def is_multiple(n,m):
+	try:
+#	if (m == 0):
+#		print ("Second number can't be zero")
+#	else:
+		if (n % m == 0):
+			print ('First number is a multiple of Second number')
+		else:
+			print ('First number is not a multiple of Second number')
+	except ZeroDivisionError as e:
+		print ("Division by zero is not allowed. Error: ", e)
+
+
+
+n = int(input('Enter first number: '))
+m = int(input('Enter second number: '))
+
+is_multiple (n,m)
+	
